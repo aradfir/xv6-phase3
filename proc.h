398107e8ab7fcde2137f508391f9018e1a074ac7
Arad Fir <aradfir@gmail.com>
2022-01-26 01:12:37 +0330

    added values for measuring to proc struct

--- proc.h
+++ proc.h
@@ -52,6 +52,9 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  int waiting_time;
+  int turnaround_time;
+  int CBT;
 };
 
 // Process memory is laid out contiguously, low addresses first:
